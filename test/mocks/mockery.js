ea09ed763bd284a2be4e02bd13b972126f5fd950
Enrico Foschi <foschi.enrico@gmail.com>
2016-02-26 07:56:46 +0100

    Removing makefile dependencies and putting npm to work with dotenv for test .env

--- test/mocks/mockery.js
+++ test/mocks/mockery.js
@@ -1,11 +1,14 @@
 import slackbots from './libraries/slackbots.js'
+import dotenv from 'dotenv';
 
 const mockery = require('mockery');
 
 const setup = ({output}) => {
-
     mockery.registerMock('slackbots', slackbots(output));
     mockery.enable({ useCleanCache: true });
+    dotenv.config({
+        path: '../'
+    });
 };
 const tearDown = () => {
     mockery.disable();
